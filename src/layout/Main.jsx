8205ea68386cd26b1fe35c47995b463f6c6e014e
hbashar434 <h.bashar434@gmail.com>
2023-05-18 18:57:17 +0600

    upto toy details

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
       <div className="my-bg-pink">
         <Navbar></Navbar>
       </div>
-      <div className="min-h-[calc(100vh-88px)] px-4">
+      <div className="min-h-[calc(100vh-88px)] px-4 md:px-0">
         <Outlet></Outlet>
       </div>
       <div className="my-bg-pink lg:px-8">
--- src/pages/ToyDetails/ToyDetails.jsx
+++ src/pages/ToyDetails/ToyDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-
+import StarRatings from "react-star-ratings";
 const ToyDetails = () => {
   const toyDetails = useLoaderData();
   const {
@@ -10,11 +10,32 @@ const ToyDetails = () => {
     sellerEmail,
     price,
     rating,
-    subcategory,
     availableQuantity,
+    description,
   } = toyDetails;
 
-  return <div></div>;
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card md:w-6/12 bg-base-100 shadow-xl">
+        <div className="bg-purple-500 text-white px-4 py-2 rounded-t-lg flex items-center">
+          <h2 className="text-2xl font-bold">{name}</h2>
+        </div>
+        <figure className="px-6 pt-6">
+          <img src={pictureURL} alt="Toy" className="rounded-xl" />
+        </figure>
+        <div className="flex justify-between px-10 pt-4">
+          
+          <p className="font-semibold text-lg">{price} $</p>
+        </div>
+        <div className="card-body pt-2">
+          <h2 className="card-title">{sellerName}</h2>
+          <p>{sellerEmail}</p>
+          <p>Quantity : {availableQuantity} pcs</p>
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ToyDetails;
