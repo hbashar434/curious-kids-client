21f81a26e645651126ba57268ab2ac7b02405b0f
hbashar434 <h.bashar434@gmail.com>
2023-10-19 01:59:07 +0600

    fixedColor

--- src/pages/ReviewSlider/ReviewSlider.jsx
+++ src/pages/ReviewSlider/ReviewSlider.jsx
@@ -4,9 +4,58 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const ReviewSlider = () => {
-  const [reviews, setReviews] = useState([]);
+const reviews = [
+  {
+    id: 1,
+    name: "John Smith",
+    img: "https://i.ibb.co/W3G35vG/person-2.jpg",
+    description:
+      "I recently bought this amazing RobotX toy for my nephew, and he absolutely loves it! The toy is highly interactive and engaging, and it keeps him entertained for hours on end.",
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: "Michael Johnson",
+    img: "https://i.ibb.co/qB28DtJ/person-7.jpg",
+    description:
+      "This RoboTech robot toy is absolutely fantastic! It's incredibly well-designed, durable, and packed with an impressive array of features. My son can't get enough of it and fun playing with it",
+    rating: 4.5,
+  },
+  {
+    id: 3,
+    name: "Daniel Lee",
+    img: "https://i.ibb.co/z62H9G7/person-4.jpg",
+    description:
+      "I purchased awesome BotBuddy robot toy as a gift for my niece, and she adores it! The toy is educational, and it helps improve her problem-solving skills while having fun.",
+    rating: 4.5,
+  },
+  {
+    id: 4,
+    name: "David Anderson",
+    img: "https://i.ibb.co/0sWFQCn/person-3.jpg",
+    description:
+      "I was skeptical at first, but this RoboRacer robot toy exceeded my expectations. The quality is top-notch, and the remote control functionality adds an extra level of excitement.",
+    rating: 4.7,
+  },
+  {
+    id: 5,
+    name: "James Wilson",
+    img: "https://i.ibb.co/ZhBKXg0/person-9.jpg",
+    description:
+      "My daughter received fantastic DinoBot robot toy for her birthday, and it's been a hit! The toy is interactive, easy to use, and sparks her imagination. Highly recommended!",
+    rating: 5,
+  },
+  {
+    id: 6,
+    name: "Daniel Thompson",
+    img: "https://i.ibb.co/r3D5Pvb/person-5.jpg",
+    description:
+      "I bought collector's edition RetroBot robot toy for myself as a collector, and it's a gem! The attention to detail is remarkable, and it's a great addition to my collection.",
+    rating: 4.8,
+  },
+];
 
+const ReviewSlider = () => {
   const SampleNextArrow = (props) => {
     const { className, style, onClick } = props;
     return (
@@ -17,7 +66,7 @@ const ReviewSlider = () => {
           display: "block",
           background: "#9384D1",
           paddingTop: "1px",
-          borderRadius: '100%'
+          borderRadius: "100%",
         }}
         onClick={onClick}
       />
@@ -34,7 +83,7 @@ const ReviewSlider = () => {
           display: "block",
           background: "#9384D1",
           paddingTop: "1px",
-          borderRadius: '100%'
+          borderRadius: "100%",
         }}
         onClick={onClick}
       />
@@ -53,12 +102,6 @@ const ReviewSlider = () => {
     prevArrow: <SamplePrevArrow />,
   };
 
-  useEffect(() => {
-    fetch("https://curious-kids-server.vercel.app/reviews")
-      .then((res) => res.json())
-      .then((data) => setReviews(data));
-  }, []);
-
   return (
     <div className="my-10 md:my-24 px-4 md:px-8">
       <div className="mb-4 md:mb-10 md:w-6/12 md:mx-auto md:text-center">
@@ -67,14 +110,15 @@ const ReviewSlider = () => {
         </h2>
         <p className="md:text-lg text-gray-700 mt-4">
           Customers are thrilled with our mesmerizing robot toys. From
-          interactive play to educational experiences, these toys deliver endless excitement.
+          interactive play to educational experiences, these toys deliver
+          endless excitement.
         </p>
       </div>
       <div className="text-center w-9/12 mx-auto">
         <Slider {...sliderSettings}>
           {reviews.map((item) => (
             <div
-              key={item?._id}
+              key={item?.id}
               className=" max-w-lg h-full md:h-60 text-start bg-white rounded-lg shadow-sm border-2 py-6 px-8"
             >
               <div className="flex items-center mb-4">
